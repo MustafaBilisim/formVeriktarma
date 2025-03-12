44f4e876f9dfe647a5fc2e542d20822af68653ed
Mustafa ARIK <mustafaarik@gmail.com>
2025-03-12 23:55:50 +0300

    Ikinci Ekran Stringler yerleştirildi. Ana ekrana Onay kutusu eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.formveriktarma"
-        minSdk = 27
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
