642e48cafb05a5c9614de70071b485c89dc0a21e
Proje5 <arikmustafa5015@gmail.com>
2025-03-12 16:39:22 +0300

    Ikinci Ekran Eklendi

--- app/src/main/java/com/example/formveriktarma/IkinciEkran.kt
+++ app/src/main/java/com/example/formveriktarma/IkinciEkran.kt
@@ -1,30 +1,20 @@
 package com.example.formveriktarma
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.formveriktarma.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+class IkinciEkran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_ikinci_ekran)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun gonder(view: View)
-    {
-        val yeniActi
     }
 }
