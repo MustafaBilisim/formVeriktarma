6bd24929c5d3c4c3507040612303f5f8a881a99e
Mustafa ARIK <mustafaarik@gmail.com>
2025-03-16 19:13:35 +0300

    Ikinci ekrana veriler gönderildi, ikinci ekranda veriler alındı. Ekrana yazdırıldı.

--- app/src/main/java/com/example/formveriktarma/IkinciEkran.kt
+++ app/src/main/java/com/example/formveriktarma/IkinciEkran.kt
@@ -1,6 +1,8 @@
 package com.example.formveriktarma
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,5 +21,26 @@ class IkinciEkran : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val ad = intent.getStringExtra("adSoyad")
+        val ogrNo = intent.getIntExtra("ogrNo",9)   // getInt komutu için varsayılan değer 9. Sınıf
+        val sinif = intent.getStringExtra("sinif")
+        val cinsiyet = intent.getStringExtra("cinsiyet")
+        val katilim = intent.getBooleanExtra("katilim",true)   // Cinsiyet için varsayılan değer erkek
+
+        binding.txt2AdSoyad.setText(ad)
+        binding.txt2OgrenciNo.setText(ogrNo.toString())
+        binding.txtCinsiyet.setText(cinsiyet)
+        binding.txtSinif.setText(sinif)
+        if(katilim) {
+            binding.txtDurum.setText("KATILIYOR")
+            binding.txtDurum.setTextColor(Color.GREEN)
+        }
+        else {
+            binding.txtDurum.setText("== KATILMIYOR == ")
+            binding.txtDurum.setTextColor(Color.RED)
+        }
+
     }
+
 }
--- app/src/main/java/com/example/formveriktarma/MainActivity.kt
+++ app/src/main/java/com/example/formveriktarma/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.formveriktarma
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,7 +27,29 @@ class MainActivity : AppCompatActivity() {
 
     fun gonder(view: View)
     {
+        val adSoyad = binding.txtAdSoyad.text.toString()
+        val ogrNo   = binding.txtOgrNo.text.toString().toInt()
+        val cinsiyet= if (binding.radioCinsErkek.isChecked)  "Erkek" else "Kadın"
+        val sinif9  = binding.radioSinif9.isChecked
+        val sinif10 = binding.radioSinif10.isChecked
+        val sinif11 = binding.radioSinif11.isChecked
+        val sinif12 = binding.radioSinif12.isChecked
+        val katilim:Boolean = binding.katilmakIstiyorum.isChecked
+        var sinif :String = "12"
+
+        if(sinif9) sinif = "9"
+        else if(sinif10) sinif = "10"
+        else if(sinif11) sinif = "11"
+        else sinif = "12"
+
+
         val IkinciEkraniAc = Intent(this,IkinciEkran::class.java)
+
+        IkinciEkraniAc.putExtra("adSoyad",adSoyad)
+        IkinciEkraniAc.putExtra("ogrNo",ogrNo)
+        IkinciEkraniAc.putExtra("sinif",sinif)
+        IkinciEkraniAc.putExtra("cinsiyet",cinsiyet)
+        IkinciEkraniAc.putExtra("katilim",katilim)
 
         startActivity(IkinciEkraniAc)
     }
